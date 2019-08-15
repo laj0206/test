8f3232084bbc70f81b98c7ba88e6a33b1cf43649
laj0206 <laj0206@gmail.com>
2019-08-14 23:26:08 -0400

    first commit

---
+++ ValueApi/Middlewares/HealthCheckMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace ValueApi.Middlewares
+{
+    public class HealthCheckMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly string _path;
+
+        public HealthCheckMiddleware(RequestDelegate next, string path)
+        {
+            _next = next;
+            _path = path;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (context.Request.Path.Value == _path)
+            {
+                await context.Response.WriteAsync("UP");
+            }
+            else
+            {
+                await this._next(context);
+            }
+        }
+    }
+
+    public static class HealthCheckMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseHealthCheck(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<HealthCheckMiddleware>("/_health");
+        }
+    }
+}
---
+++ ValueApi/README.md
@@ -0,0 +1,6 @@
+﻿Just a test repo to learn aws setup
+
+1. health check
+2. end point
+3. add database
+4. add auth
